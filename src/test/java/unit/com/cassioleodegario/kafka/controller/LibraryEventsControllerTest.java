74cf9e30d8f36b037e4b8580a7773f799ecdf659
CassioLeodegario <cassio.leodegario@gmail.com>
2021-10-17 10:05:55 -0300

    test: ensure controller returns 400 when posting invalid body

--- src/test/java/unit/com/cassioleodegario/kafka/controller/LibraryEventsControllerTest.java
+++ src/test/java/unit/com/cassioleodegario/kafka/controller/LibraryEventsControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(LibraryEventsController.class)
@@ -51,5 +52,30 @@ public class LibraryEventsControllerTest {
                 .content(json)
                 .contentType(MediaType.APPLICATION_JSON))
                     .andExpect(status().isCreated());
+    }
+
+    @Test
+    void postLibraryEvent_4xx() throws Exception {
+        //given
+        Book book = Book.builder()
+                .bookId(null)
+                .bookName(null)
+                .bookAuthor("Test Author")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(null)
+                .book(book)
+                .build();
+
+        String json = objectMapper.writeValueAsString(libraryEvent);
+        doNothing().when(libraryEventProducer).sendLibraryEvent_Approach2(isA(LibraryEvent.class));
+        //expect
+        String expectedErrorMessage = "book.bookId - Book id cannot be null, book.bookName - Book name cannot be empty";
+        mockMvc.perform(post("/v1/libraryevent")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError())
+        .andExpect(content().string(expectedErrorMessage));
     }
 }
